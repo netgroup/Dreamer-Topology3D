b1308b5ce866971f190193663e406e680144d2eb
Francesco Lombardo <franclombardo@gmail.com>
2015-12-09 14:08:06 +0100

    integration adminlte clean css obsoleti

---
+++ js/src/dreamerUI.js
@@ -0,0 +1,23 @@
+if (typeof dreamer === 'undefined') {
+    var dreamer = {};
+}
+
+dreamer.DreamerUI = (function (global) {
+    'use strict';
+
+    function DreamerUI () {
+
+        
+    }
+
+    DreamerUI.prototype.example = function (layer) {
+       
+    };
+
+
+    return DreamerUI;
+}());
+
+if (typeof module === 'object') {
+    module.exports = dreamer.DreamerUI;
+}
